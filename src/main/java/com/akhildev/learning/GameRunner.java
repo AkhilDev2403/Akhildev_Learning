846be104a30c888010b1bc12519d0073a776b568
AkhilDev2403 <akhildev2403@gmail.com>
2023-06-19 18:58:49 +0530

    test

--- src/main/java/com/akhildev/learning/GameRunner.java
+++ src/main/java/com/akhildev/learning/GameRunner.java
@@ -42,6 +42,7 @@ import org.springframework.stereotype.Component;
 //		consoleInterface.right();
 //	}
 //	
+//
 //}
 
 
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sayone
+ *
+ */
+module test {
+}
